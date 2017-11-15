29416c53ec361e0bc615c24ecabd8ad2f10bd5f3
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-11-15 13:06:19 -0700

    added withdraw functionality

--- src/Account.js
+++ src/Account.js
@@ -9,6 +9,7 @@ export default class Account extends Component {
     }
 
     this.handleDepositClick = this.handleDepositClick.bind(this)
+    this.handleWithdrawClick = this.handleWithdrawClick.bind(this)
   }
 
   handleDepositClick(e) {
@@ -26,6 +27,24 @@ export default class Account extends Component {
     }
   }
 
+  handleWithdrawClick(e) {
+    e.preventDefault();
+    if (isNaN(this.refs.amount.value)) {
+      console.log("Not a number!");
+    } else {
+      if (this.state.balance >= 0 && this.refs.amount.value <= this.state.balance) {
+        let amount = +this.refs.amount.value;
+        let newBalance = this.state.balance - amount;
+        this.setState({
+          balance: newBalance
+        })
+        this.refs.amount.value = '';
+      } else {
+        alert('You do not have that much money available')
+      }
+    }
+  }
+
   render() {
     let balanceClass = 'balance';
     if (this.state.balance === 0) {
@@ -38,6 +57,7 @@ export default class Account extends Component {
         <div className={balanceClass}>${this.state.balance}</div>
         <input type="text" placeholder="enter an amount" ref="amount" />
         <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+        <input type="button" value="Withdraw" onClick={this.handleWithdrawClick} />
       </div>
     )
   }
